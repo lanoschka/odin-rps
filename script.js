0d94985d14d76ce108e857633975e092e25c2ed3
lanoschka <129803874+lanoschka@users.noreply.github.com>
2023-12-16 20:42:06 +0100

    Created function that plays one round and tells who won

--- script.js
+++ script.js
@@ -5,5 +5,29 @@ function getComputerChoice(){
 }
 
 //test the function
-const computerChoice = getComputerChoice();
-console.log("Computer's choice: " + computerChoice);
+console.log("Computer's choice: " + getComputerChoice());
+
+const playerSelection = prompt("Select Rock, Paper or Scissors").toLowerCase();
+const computerSelection = getComputerChoice().toLowerCase();
+
+function playRound(playerSelection, computerSelection){
+
+    if(playerSelection === computerSelection){
+       
+        return "It's a tie! Replay the round.";
+        
+    }else if(
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ){
+        return 'You win! ' + playerSelection + ' beats ' + computerSelection + '.';
+    }else {
+        return 'You lose! ' + computerSelection + ' beats ' + playerSelection + '.';
+    }
+   
+
+}
+
+//test the functiomn
+console.log(playRound(playerSelection, computerSelection));

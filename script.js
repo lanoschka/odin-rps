44a23144ae335551ae59f98d8f9de8ad3bc66e09
lanoschka <129803874+lanoschka@users.noreply.github.com>
2023-12-26 20:56:07 +0100

    Created function game()which starts function playRound() until one of the players wins BestOfFive

--- script.js
+++ script.js
@@ -5,29 +5,51 @@ function getComputerChoice(){
 }
 
 //test the function
-console.log("Computer's choice: " + getComputerChoice());
+//console.log("Computer's choice: " + getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 
-const playerSelection = prompt("Select Rock, Paper or Scissors").toLowerCase();
-const computerSelection = getComputerChoice().toLowerCase();
-
-function playRound(playerSelection, computerSelection){
-
-    if(playerSelection === computerSelection){
-       
+    if (playerSelection === computerSelection) {
         return "It's a tie! Replay the round.";
-        
-    }else if(
+    } else if (
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')
-    ){
+    ) {
         return 'You win! ' + playerSelection + ' beats ' + computerSelection + '.';
-    }else {
+    } else {
         return 'You lose! ' + computerSelection + ' beats ' + playerSelection + '.';
     }
-   
-
 }
 
-//test the functiomn
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    while (playerScore < 3 && computerScore < 3) {
+        const playerSelection = prompt("Enter your choice (Rock, Paper, or Scissors):");
+        const computerSelection = getComputerChoice();
+
+        const roundResult = playRound(playerSelection, computerSelection);
+
+        console.log(roundResult);
+
+        if (roundResult.includes('win')) {
+            playerScore++;
+        } else if (roundResult.includes('lose')) {
+            computerScore++;
+        }
+    }
+
+    if (playerScore > computerScore) {
+        console.log('You win the game! Final score: ' + playerScore + '-' + computerScore);
+    } else if (playerScore < computerScore) {
+        console.log('You lose the game! Final score: ' + playerScore + '-' + computerScore);
+    } else {
+        console.log('It\'s a tie! Final score: ' + playerScore + '-' + computerScore);
+    }
+}
+
+// Test the game
+game();
